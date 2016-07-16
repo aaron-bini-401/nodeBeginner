2b6c98e1bc0afa082a11269e7ff7482bb2ec7b32
Aaron Bini <bini.aaron@gmail.com>
2016-07-15 18:06:02 -0700

    finished beginner-node tutorial

---
+++ index.js
@@ -0,0 +1,12 @@
+const server = require('./server');
+const router = require('./router');
+const requestHandler = require('./requestHandler');
+
+var handle = {};
+handle['/'] = requestHandler.start;
+handle['/start'] = requestHandler.start;
+handle['/upload'] = requestHandler.upload;
+handle['/show'] = requestHandler.show;
+
+
+server.start(router.route, handle);
